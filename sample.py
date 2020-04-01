25989f6ca32cf5b3acdb9cd3ff81e67bfbb6b7dc
VPetsev <59983319+VPetsev@users.noreply.github.com>
2020-04-01 16:47:57 -0500

    Add files via upload

---
+++ sample.py
@@ -0,0 +1,16 @@
+# name is a variable, underscores can be used to seperate words
+# variables temporarily store data in the memory
+first_name = input('What is your first name? ')
+last_name = input('What is your last name? ')
+middle_name = 'Danger'
+
+# 'print' is a functon with with "()"
+# print()
+
+# Arguent goes inside the "()"
+# print('hello world')
+
+print(first_name)
+print(middle_name)
+print(last_name)
+
---
+++ string-intepolation.py
@@ -0,0 +1,3 @@
+first_name = input('Please enter your first name: ')
+last_name = input('Please enter your last name: ')
+print(f"Hello, My Name is {first_name}, {last_name}")
